5776c96d6a4f343b21aa85b152e4824f98b9a99c
wallago <45556867+wallago@users.noreply.github.com>
2026-08-15 23:14:53 +0200

    refactor(flake/module): switch from nixos to hm

--- flake.nix
+++ flake.nix
@@ -22,8 +22,7 @@
     }:
     # ── System-agnostic outputs (modules) live out here ──
     {
-      # nixosModules.default = import ./nix/module.nix self;
-      # homeModules.default = import ./nix/hm-module.nix self;
+      homeModules.default = import ./nix/hm-module.nix self;
     }
     # ── Then merge the per-system outputs onto it ──
     //
